f398cd47ebbb7cce8fb4a0d29ac419fcc5f18eb8
Rishi Rami <rishirami876@gmail.com>
2025-07-16 21:03:06 +0530

    Backup: updated _JAVA\add.java at 2025-07-16 21:03:06.613651

---
+++ _JAVA/add.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class add {
+    public static void main(String[] args) {
+        
+        int a,b;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter value of a ");
+
+        a = sc.nextInt();
+
+        System.out.println("Enter value of b ");
+
+        b = sc.nextInt();
+
+        int sum = a+b; //this is it dude
+
+        System.out.println("The sum of a and b is "+sum); 
+    }
+}
