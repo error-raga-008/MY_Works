d2621592d2fbef7e7f9c7a57f5be52bd90ec3e13
Rishi Rami <rishirami876@gmail.com>
2025-07-20 20:59:13 +0530

    Backup: updated _JAVA\add.java at 2025-07-20 20:59:13.132937

--- _JAVA/add.java
+++ _JAVA/add.java
@@ -18,6 +18,22 @@ public class add {
         int sum = a+b; //this is it dude
 
         System.out.println("The sum of a and b is "+sum); 
+
+
+        //rest of the arthematic calculations
+        int add = a + b;
+        System.out.println(add);
+
+        int sub = a-b;
+        System.out.println(sub);
+
+        int multi = a*b;
+        System.out.println(multi);
+
+        int div = a/b;
+        System.out.println(div);
+
+        int mod = a%b;
+        System.out.println(mod);
     }
 }
-
