35d54782bb4468b442e27b66e346630933e19123
Rishi Rami <rishirami876@gmail.com>
2025-07-20 20:59:13 +0530

    Backup: updated _JAVA\strings.java at 2025-07-20 20:59:13.316770

---
+++ _JAVA/strings.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String");
+        String a = sc.nextLine(); //String needes to written like this 
+
+        System.out.println("this i a outputed string " + a);
+
+        System.out.println("Enter a char datatype");
+        char b = 'g';// char can only hold one character in  
+    }    
+}
