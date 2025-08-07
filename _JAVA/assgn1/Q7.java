57cddf520f3f30c81944c811c72f4c31531f2dcf
Rishi Rami <rishirami876@gmail.com>
2025-08-07 22:08:20 +0530

    Backup: updated _JAVA\assgn1\Q7.java at 2025-08-07 22:08:20.375671

---
+++ _JAVA/assgn1/Q7.java
@@ -0,0 +1,17 @@
+public class Q7 {
+  
+    Q7(){
+        int l = 1;
+        int w = 2;
+
+        System.out.println("default cons "+(l*w));
+    }
+    Q7(int w,int l){
+
+        System.out.println("para cons "+(l*w));
+    }
+    public static void main(String[] args) {
+        Q7 q = new Q7();
+        Q7 a = new Q7(1,3);
+    }
+}
