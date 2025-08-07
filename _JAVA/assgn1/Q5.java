f0c31f108d9f05d52e878d44d211ceedfd9c4df7
Rishi Rami <rishirami876@gmail.com>
2025-08-07 22:08:20 +0530

    Backup: updated _JAVA\assgn1\Q5.java at 2025-08-07 22:08:20.007917

---
+++ _JAVA/assgn1/Q5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q5 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter num");
+        int x = sc.nextInt();
+
+        int z = 0,y;
+        
+        while (x != 0) {
+            y = x%10;
+            z += y;
+            x /= 10;
+        }
+
+        System.out.println("the sum of the number is "+z);
+    }
+
+}
