6702651708c46a0493d9eeb8c2b0fba4e3ba73f7
Rishi Rami <rishirami876@gmail.com>
2025-08-07 22:08:20 +0530

    Backup: updated _JAVA\assgn1\Q6.java at 2025-08-07 22:08:20.190526

---
+++ _JAVA/assgn1/Q6.java
@@ -0,0 +1,17 @@
+public class Q6 {
+    public static void main(String[] args) {
+        
+        int a = 1;
+
+        double d = 44.2;
+
+        System.out.println("implicit converstion "+(a+d));
+
+        double x = a+d;
+
+        int y = (int) x;
+
+        System.out.println("explicit converstion "+(y));
+
+    }
+}
