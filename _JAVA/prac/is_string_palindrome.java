a08f06e980a87691f39666aa7624f45871169402
Rishi Rami <rishirami876@gmail.com>
2025-08-20 06:51:21 +0530

    Backup: updated _JAVA\prac\is_string_palindrome.java at 2025-08-20 06:51:21.015266

---
+++ _JAVA/prac/is_string_palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        String x = "NAUN";
+
+        int n = x.length();
+
+        System.out.println(n);
+
+        for (int i = 0; i < n/2; i++) {
+            
+            if (x.charAt(i)!= x.charAt(n - i -1)) {
+                System.out.println("NOP");
+                break;
+            }
+
+        }
+        System.out.println("DONE");
+
+    }
+}
