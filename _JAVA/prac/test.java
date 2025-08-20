70346aad369c45eb4521bf358de9a0c45a072814
Rishi Rami <rishirami876@gmail.com>
2025-08-20 06:51:23 +0530

    Backup: updated _JAVA\prac\test.java at 2025-08-20 06:51:23.427539

---
+++ _JAVA/prac/test.java
@@ -0,0 +1,11 @@
+import mypack.MATHHELP;
+
+public class test {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int c = 2;
+
+        System.out.println(add(a,c));
+    }
+}
