7a05ed6824c6b8012c0b4ab360517ce8b34d2189
Rishi Rami <rishirami876@gmail.com>
2025-08-20 06:51:18 +0530

    Backup: updated _JAVA\pack_test.java at 2025-08-20 06:51:18.560859

---
+++ _JAVA/pack_test.java
@@ -0,0 +1,12 @@
+import mypack.MATHHELP;
+
+public class pack_test {
+
+    public static void main(String[] args) {
+        MATHHELP hh = new MATHHELP();
+        int a = 1;
+        int c = 2;
+
+        System.out.println(hh.add(a,c));
+    }
+}
