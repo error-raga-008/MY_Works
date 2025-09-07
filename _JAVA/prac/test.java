1528bafc29d3e94b4ccf8cbda216186dc9cde29c
Rishi Rami <rishirami876@gmail.com>
2025-09-07 19:07:46 +0530

    Backup: updated _JAVA\prac\test.java at 2025-09-07 19:07:46.541622

--- _JAVA/prac/test.java
+++ _JAVA/prac/test.java
@@ -1,11 +1,24 @@
-import mypack.MATHHELP;
+import java.util.Scanner;
 
 public class test {
 
     public static void main(String[] args) {
-        int a = 1;
-        int c = 2;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(add(a,c));
+        int[][] a = new int[2][2];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
     }
 }
