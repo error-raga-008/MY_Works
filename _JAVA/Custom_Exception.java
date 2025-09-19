d603b161598e22a36c552a343242ebf3433177de
Rishi Rami <rishirami876@gmail.com>
2025-09-19 19:11:38 +0530

    Backup: updated _JAVA\Custom_Exception.java at 2025-09-19 19:11:38.572939

---
+++ _JAVA/Custom_Exception.java
@@ -0,0 +1,26 @@
+//to make a Custom Exception we do this shit
+
+class InvalidAgeException extends Exception{ //we need to extends the coustom excep to the Exception class
+    InvalidAgeException(String s){ //next make a constructor to with a string perimeter
+        super(s);// Pass the string to Exception constructor
+    }
+}
+public class Custom_Exception {
+    static void validate(int age)throws InvalidAgeException{ //making a fuction to check the things
+        if (age<18) {
+            throw new InvalidAgeException("not valid to vote");
+        }
+        else{
+            System.out.println("welcome");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            validate(12);
+        } catch (Exception e) {
+            System.out.println("Exception occured"+e);
+        }
+        System.out.println("DONE");
+    }
+}
