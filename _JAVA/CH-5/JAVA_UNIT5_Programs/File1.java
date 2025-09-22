72155f6c7f8ff24983290ad49ec5c73056e8cfb5
Rishi Rami <rishirami876@gmail.com>
2025-09-22 21:52:45 +0530

    Backup: updated _JAVA\CH-5\JAVA_UNIT5_Programs\File1.java at 2025-09-22 21:52:45.371601

---
+++ _JAVA/CH-5/JAVA_UNIT5_Programs/File1.java
@@ -0,0 +1,13 @@
+// Importing File Class
+import java.io.*;
+
+class File1 
+{
+    public static void main(String[] args)
+    {
+        // File name specified
+            File Obj = new File("File1.txt");
+	    
+        System.out.println("File Created!");
+    }
+}
