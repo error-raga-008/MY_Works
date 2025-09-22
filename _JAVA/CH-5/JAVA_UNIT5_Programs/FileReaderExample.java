a1ffce9e871c6e9aac90527b681c13396fbf3afd
Rishi Rami <rishirami876@gmail.com>
2025-09-22 21:52:46 +0530

    Backup: updated _JAVA\CH-5\JAVA_UNIT5_Programs\FileReaderExample.java at 2025-09-22 21:52:45.964251

---
+++ _JAVA/CH-5/JAVA_UNIT5_Programs/FileReaderExample.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class FileReaderExample {
+    public static void main(String[] args) {
+        try {
+		
+            FileReader fr = new FileReader("input.txt");
+            int ch;
+            while ((ch = fr.read()) != -1) {
+                System.out.print((char) ch);  // convert int to char
+            }
+            fr.close();
+	    
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
