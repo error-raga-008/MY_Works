0b484132c8139c2b8876f62f5c71f9d84302e5a1
Rishi Rami <rishirami876@gmail.com>
2025-09-22 21:52:42 +0530

    Backup: updated _JAVA\CH-5\File_InputStream_and_OutputStream.java at 2025-09-22 21:52:42.350473

---
+++ _JAVA/CH-5/File_InputStream_and_OutputStream.java
@@ -0,0 +1,39 @@
+import java.io.*;
+public class File_InputStream_and_OutputStream {
+    public static void main(String[] args){
+
+        //FileReader reads the char from a file and FileWriter wrties char to a file
+        //FileInputStream reads the byte from a file and FileOutputStream writes byte to a file
+
+        try { //yes this shit will also dont without this
+            FileInputStream ined = new FileInputStream("source.txt");
+            FileOutputStream outed = new FileOutputStream("desitination.txt");
+
+            int c;
+
+            while ((c=ined.read()) != -1) {
+                System.out.println(c);
+            }
+
+            //FileInputStream Methods
+
+            ined.read();
+            ined.read(null);
+            ined.available();
+            
+
+            //FileOutputStream Methods
+
+            outed.write(1);
+            outed.write(null);
+            outed.write(null, c, c);
+            outed.flush();
+
+            ined.close();
+            outed.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
