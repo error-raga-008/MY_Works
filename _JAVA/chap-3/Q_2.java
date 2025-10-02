66e3f508a61b77b807463cb1703e51cf75c1a8ea
Rishi Rami <rishirami876@gmail.com>
2025-10-02 12:59:20 +0530

    Backup: updated _JAVA\chap-3\Q_2.java at 2025-10-02 12:59:20.412077

---
+++ _JAVA/chap-3/Q_2.java
@@ -0,0 +1,27 @@
+class veh{
+
+    String brand = "FORD";
+    int year = 2020;
+}
+
+class car extends veh{
+
+    String model="bronco";
+}
+
+class elecar extends car{
+
+    int batcap = 111;
+
+    void out(){
+        System.out.println("year = "+year+"\nbrand = "+brand+"\nmodel = "+model+"\nbatcap = "+batcap);
+    }
+}
+
+public class Q_2 {
+    
+    public static void main(String[] args) {
+        elecar e = new elecar();
+        e.out();
+    }
+}
