09e5e319d69012c034798a68a78e7cfaade3f698
Rishi Rami <rishirami876@gmail.com>
2025-10-05 13:51:42 +0530

    Backup: updated _JAVA\test.java at 2025-10-05 13:51:42.944135

--- _JAVA/test.java
+++ _JAVA/test.java
@@ -1,17 +1,12 @@
-import java.util.*;
-
 public class test {
+
     public static void main(String[] args) {
+        int[] x ={1,2};
         
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a String");
-        String a = sc.nextLine(); //String needes to written like this 
-
-        System.out.println("this i a outputed string " + a);
-
-        System.out.println("Enter a char datatype");
-        char b = 'g';// char can only hold one character in  
-    }    
+        try {
+            System.out.println(x[3]);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
-
