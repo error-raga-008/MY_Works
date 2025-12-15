1c2b0c1895da7a4b728b3322dbbedad12ed995bf
Rishi Rami <rishirami876@gmail.com>
2025-12-15 22:30:19 +0530

    Backup: updated _JAVA\prac\ch-2\string iteration for 10000 times.java at 2025-12-15 22:30:19.735515

---
+++ _JAVA/prac/ch-2/string iteration for 10000 times.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder x = new StringBuilder("ola");
+        StringBuffer y = new StringBuffer("oa");
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            x .append("x");
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("for buffer"+( end-start ));
+
+        start = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            y.append("x");
+        }
+
+        end = System.currentTimeMillis();
+
+        System.out.println("for builder"+( end-start ));
+    }    
+}
