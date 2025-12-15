b748209454546a57098e2427b664bd2f54449805
Rishi Rami <rishirami876@gmail.com>
2025-12-15 22:30:22 +0530

    Backup: updated _JAVA\prac\ch-3\super.java at 2025-12-15 22:30:22.023540

---
+++ _JAVA/prac/ch-3/super.java
@@ -0,0 +1,18 @@
+//to call the constructor using super we need to put it in the child class constructor
+
+class shape {
+    shape(int x){
+        System.out.println("elo");
+    }
+}
+class cir extends shape {
+    cir(){
+        super(1);
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+        cir c = new cir();
+    }    
+}
