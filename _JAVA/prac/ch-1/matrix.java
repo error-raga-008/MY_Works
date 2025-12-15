a5d25e42e656186885e6421817f41a53e0a33dcd
Rishi Rami <rishirami876@gmail.com>
2025-12-15 22:30:18 +0530

    Backup: updated _JAVA\prac\ch-1\matrix.java at 2025-12-15 22:30:18.494326

---
+++ _JAVA/prac/ch-1/matrix.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[2][2];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
