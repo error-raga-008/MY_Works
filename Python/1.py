1c6f3320ff4eb9f30a5b048a516d16ceccc2cbbc
raga <rishirami876@gmail.com>
2025-12-23 22:45:13 +0530

    Backup: updated Python\1.py at 2025-12-23 22:45:13.317245

---
+++ Python/1.py
@@ -0,0 +1,20 @@
+print("Hello world\n")
+
+a = 1
+b = 2
+
+print(a+b,"damm boi\n")
+
+x = float(input("Enter some shit "))
+
+print(x)
+
+x = int(a)
+
+print("\n",type(a))
+
+x = True #the true and flase need to have capital "T AND F" in python 
+
+# membership ops
+# assignment ops
+# logical ops
