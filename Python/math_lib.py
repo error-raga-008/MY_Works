1f554051e635949d355ff0c38790c7473a136f15
raga <rishirami876@gmail.com>
2025-12-23 23:39:46 +0530

    Backup: updated Python\math_lib.py at 2025-12-23 23:39:46.018109

---
+++ Python/math_lib.py
@@ -0,0 +1,19 @@
+import math as m
+a=1.1
+
+print(m.pi) #print value of pie
+
+print(a.__pow__(2)) #a power 2
+x = m.pow(2,3) #can also be used 2 to power 3
+print(x)
+
+x = m.factorial(5) #print factorial of 5
+print(x)
+
+print(m.e) #print value of e
+
+print(m.fmod(1,2)) #Returns the remainder of x/y
+
+print(a.__floor__()) #roundup the float to the smallest near int
+
+print(a.__ceil__()) #roundup the float to the largest near int
