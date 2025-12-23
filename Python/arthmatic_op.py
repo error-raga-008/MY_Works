61e5f5742f2a752832548edaa6ca1ac55f7f8981
raga <rishirami876@gmail.com>
2025-12-23 22:45:13 +0530

    Backup: updated Python\arthmatic_op.py at 2025-12-23 22:45:13.796310

---
+++ Python/arthmatic_op.py
@@ -0,0 +1,23 @@
+a = 10
+b = 20
+
+# addtion
+print(a+b,"\n")
+
+# subraction
+print(a-b,"\n")
+
+# multi
+print(a*b,"\n")
+
+# exponets
+print(a**b,"\n") #a raise to b
+
+#modulus
+print(a%b,"\n")
+
+# division
+print(a/b,"\n")
+
+# floor division
+print(a//b,"\n") #divides two numbers and rounds the result down to the nearest integer
