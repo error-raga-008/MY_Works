631d38b75b761cee89e8d3d99b3c0a6c4b017f75
raga <rishirami876@gmail.com>
2025-12-23 22:45:13 +0530

    Backup: updated Python\all_extra_operators.py at 2025-12-23 22:45:13.475563

---
+++ Python/all_extra_operators.py
@@ -0,0 +1,35 @@
+#ternary operator
+#Identity Operators
+#BITWISE Operators
+
+#------------------------------------------------------------------------------------------------------------------------#
+a=10
+b=20
+
+#ternary operator
+
+#value_if_true if condition else value_if_false
+
+x = True if a>b else False
+print(x,"\n")
+
+#------------------------------------------------------------------------------------------------------------------------#
+
+#Identity Operators
+
+y = [10,20,30]
+
+print(y[1] is 10)
+print(y[1] is not 10)
+
+#------------------------------------------------------------------------------------------------------------------------#
+
+#BITWISE OPERATOR IS ONYL THEORY
+
+# Operator	Description	        Syntax
+# &	    Bitwise AND	            x & y
+# |	    Bitwise OR	            x | y
+# ~	    Bitwise NOT	            ~x
+# ^	    Bitwise XOR	            x ^ y
+# >>	Bitwise right_shift	    x>>
+# <<	Bitwise left_shift	    x<<
