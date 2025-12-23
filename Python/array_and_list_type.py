70acf1ddb26909ddff924fb3624aa28ee7151973
raga <rishirami876@gmail.com>
2025-12-23 22:45:13 +0530

    Backup: updated Python\array_and_list_type.py at 2025-12-23 22:45:13.633560

---
+++ Python/array_and_list_type.py
@@ -0,0 +1,30 @@
+#list
+
+#list is made from []
+#list can contain diff
+#list is mutable means it we can change element later 
+a = [1,2.2,"well o well"]
+
+print(a)
+
+print(a[1]) #index works same as all
+print(a[-1]) # -1 will print it in reverse order
+print(a[0],[2]) #print from 0 to 2 but not 2 index
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
+#tuple
+
+#its not mutable and works and acts same as the list
+#its made from ()
+b = (1,2.2,"wow")
+
+print(b)
+
+print(b[1])
+print(b[-1])
+print(b[0],[2])
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
+#FOR DICTIONARIES SEE THE NIELTS NOTES I AM TOO LAzY TO WRITE IT NOW
