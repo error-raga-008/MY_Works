0397e7e0d817a5f4991d008c10749fa136fc507d
raga <rishirami876@gmail.com>
2025-12-27 23:16:25 +0530

    Backup: updated DSA\check_sparse_matrix.cpp at 2025-12-27 23:16:25.725432

---
+++ DSA/check_sparse_matrix.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int zero = 0,x = 0;
+
+    int arr[3][3]={{1,0,0},
+                    {2,1,0},
+                    {6,0,0}};
+
+    //to check if the array is spares or not
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (arr[i][j] == 0)
+            {
+                zero++;
+            }
+            else
+            {
+                x++;
+            }
+            
+            
+        }
+        
+    }
+
+    if (zero>x)
+    {
+        cout<<"Its a sparse matrix";
+    }
+    else
+    {
+        cout<<"Its not a sparse matrix";
+    }
+    
+    
+    
+
+    return 0;
+}
