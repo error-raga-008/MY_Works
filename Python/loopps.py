d4ba8379ff6b29ebfa15beb191f45013c0f9e2c7
raga <rishirami876@gmail.com>
2026-01-18 23:04:43 +0530

    Backup: updated Python\loopps.py at 2026-01-18 23:04:43.946026

---
+++ Python/loopps.py
@@ -0,0 +1,19 @@
+a = 1
+
+while a <= 10:
+    print(a)
+    a += 1
+
+for a in range(15):
+    if a == 12:
+        continue #we know what break does
+    print(a)
+    a += 1
+
+# for a on range(start,before,step) 
+
+for i in range(1,6):
+    print(i)
+
+for i in range(1,11,2):
+    print(i)
