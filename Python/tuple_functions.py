b1687f9d3e0934b21d59ab821ae782c11bfc6958
raga <rishirami876@gmail.com>
2026-01-27 22:57:12 +0530

    Backup: updated Python\tuple_functions.py at 2026-01-27 22:57:12.691514

---
+++ Python/tuple_functions.py
@@ -0,0 +1,8 @@
+#tupel methods
+
+a = (1,2,3,4)
+
+print(a.count(1)) #count the number of given element in the tuple
+
+
+print(a.index(1)) #Searches the tuple for a specified value and returns the position of where it was found
