d51054046999a2228efff3166b284a4d7942bcc5
raga <rishirami876@gmail.com>
2026-01-27 22:57:12 +0530

    Backup: updated Python\list_functions.py at 2026-01-27 22:57:12.213380

---
+++ Python/list_functions.py
@@ -0,0 +1,34 @@
+#lsit and it functions
+
+a = [1,2,3,4,5,6]
+
+b = ["A","B","C","D"]
+
+a.append(7) #add element to the last of the list
+
+c = a.copy() #make a copy of list a
+
+a.insert(1,2) #Inserts an element at a specified position
+# Insert 2 at index 1
+
+print(a.pop()) #pop a the last element or specified element from the list
+a.pop(2)
+
+c.clear() #remove all the element from the list
+
+print(a.index(1)) #print the element at the given index
+
+
+print(a.count()) #count the number of element in the list
+
+a.reverse() #reverse the list of the element
+
+a.sort() #sort the list
+
+a.extend(1) #add the element to the last of the list
+#DOES NOT WORK
+
+# print(a.max())
+# print(a.min()) #print the max and min element of the list
+
+#print(a.sum()) #print the sum of the list
