4bb2d5123e179556e3d63336d37e343dafff5cab
error-raga-008 <rishirami876@gmail.com>
2026-02-15 00:59:20 +0530

    Backup: updated DSA\test.cpp at 2026-02-15 00:59:20.830885

---
+++ DSA/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int c = 0;
+    
+    while (c < 6)
+    {
+        c++;
+    }
+    cout<<c;
+    
+
+    return 0;
+}
