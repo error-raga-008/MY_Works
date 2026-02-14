6a4061ef29a4d71332ab909cd957aa3c5ba15738
error-raga-008 <rishirami876@gmail.com>
2026-02-14 21:47:19 +0530

    Backup: updated DSA\doubly_linkedlist.cpp at 2026-02-14 21:47:18.991340

---
+++ DSA/doubly_linkedlist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class node{
+    public:
+        node *next;
+        node *prev;
+        int data;
+};
+
+node *start = NULL;
+node *rear = NULL;
+
+void create(){
+    
+}
